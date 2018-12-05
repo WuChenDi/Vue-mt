b1ef60749db536da8d7ff0ff07e39511c37e9059
WuChenDi <996194720@qq.com>
2018-12-05 21:54:49 +0800

    update code

---
+++ components/index/menu.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd
+        v-for="(item,idx) in menu"
+        :key="idx"
+      >
+        <i :class="item.type" />{{ item.name }}<span class="arrow" />
+      </dd>
+    </dl>
+    <!-- <div class="detail">
+      <template v-for="(item,idx) in curdetail.child">
+        <h4 :key="idx">{{ item.title }}</h4>
+        <span
+          v-for="v in item.child"
+          :key="v"
+        >{{ v }}</span>
+      </template>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          type: "food",
+          name: "美食",
+          child: [
+            {
+              title: "美食",
+              child: ["代金券", "甜点饮品", "火锅", "自助餐", "小吃快餐"]
+            }
+          ]
+        },
+        {
+          type: "takeout",
+          name: "外卖",
+          child: [
+            {
+              title: "外卖",
+              child: ["美团外卖"]
+            }
+          ]
+        },
+        {
+          type: "hotel",
+          name: "酒店",
+          child: [
+            {
+              title: "酒店星级",
+              child: ["经济型", "舒适/三星", "高档/四星", "豪华/五星"]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
