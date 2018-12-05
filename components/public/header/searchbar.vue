742a137bf0c725f96b4eeeb814b8569cda2bf993
WuChenDi <996194720@qq.com>
2018-12-05 20:54:31 +0800

    update search

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -14,7 +14,75 @@
         :span="15"
         class="center"
       >
-        2
+        <div class="wrapper">
+          <el-input
+            v-model="search"
+            placeholder="搜索商家或地点"
+            @focus="focus"
+            @blur="blur"
+          />
+          <button class="el-button el-button--primary"><i class="el-icon-search" /></button>
+          <dl
+            v-if="isHotPlace"
+            class="hotPlace"
+          >
+            <dt>热门搜索</dt>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+          <dl
+            v-if="isSearchList"
+            class="searchList"
+          >
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+        </div>
+        <p class="suggset">
+          <a href="#">故宫博物院</a>
+          <a href="#">故宫博物院</a>
+          <a href="#">故宫博物院</a>
+          <a href="#">故宫博物院</a>
+          <a href="#">故宫博物院</a>
+        </p>
+        <ul class="nav">
+          <li>
+            <nuxt-link
+              to="/"
+              class="takeout"
+            >美团外卖</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="movie"
+            >猫眼电影</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="hotel"
+            >美团酒店</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="apartment"
+            >民宿/公寓</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link
+              to="/"
+              class="business"
+            >商家入驻</nuxt-link>
+          </li>
+        </ul>
       </el-col>
       <el-col
         :span="6"
@@ -37,7 +105,30 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      search: "",
+      isFocus: false
+    };
+  },
+  computed: {
+    isHotPlace: function() {
+      return this.isFocus && !this.search;
+    },
+    isSearchList: function() {
+      return this.isFocus && this.search;
+    }
+  },
+  methods: {
+    focus: function() {
+      this.isFocus = true;
+    },
+    blur: function() {
+      this.isFocus = false;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
